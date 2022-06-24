3cf57e0e6d89d27b883644b08e9582ba913bb98b
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-06-24 20:49:00 +0530

    rename file

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const http = require('http');
 
-const handleRequests = require('./routes');
+const handleRequestRoutes = require('./routes');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  handleRequests(req, res);
+  handleRequestRoutes(req, res);
 });
 
 server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
