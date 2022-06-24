c0c12a79218c03bd681faf5f83e0386507ee2618
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-06-24 19:55:47 +0530

    clean up

--- index.js
+++
@@ -1,7 +0,0 @@
-// node code modules -> http, https, fs, path, os
-const fs = require('fs');
-
-// write to a file in local sys
-fs.writeFileSync('hello.txt', 'Hello from node.js');
-
-console.log('hello from node.js');
