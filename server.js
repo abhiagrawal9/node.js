4f01b51139e9134e29a8c9fafc2cc376b77a4fa9
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-06-24 10:07:05 +0530

    reading request

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
 const http = require('http');
+const fs = require('fs');
 
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  //   console.log(req.url); // -> /
-  //   console.log(req.method); // -> GET
-  //   console.log(req.headers); // -> headers
-  //   // process.exit()
+  const url = req.url;
 
-  res.setHeader('Content-Type', 'text/html');
-  res.write('<h1>Hello from node.js server</h1>');
-  res.end();
+  if (url === '/') {
+    res.write('<html>');
+    res.write(
+      '<body> <form action="/message" method="POST"> <input type="text" name="message"> <input type="submit" value="Submit"> </form> </body>'
+    );
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/message' && req.method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFileSync('hello.txt', message);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end();
+  }
 });
 
 server.listen(PORT, () => {
