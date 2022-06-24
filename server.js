90f5db88678257e261df439c96d423fd40af371e
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-06-24 08:44:15 +0530

    response

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const http = require('http');
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  //   console.log(req.url); // -> /
+  //   console.log(req.method); // -> GET
+  //   console.log(req.headers); // -> headers
+  //   // process.exit()
+
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<h1>Hello from node.js server</h1>');
+  res.end();
 });
 
 server.listen(PORT, () => {
