513249cec7e94049ee847d3a2aeeaccf043cfbaf
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-06-25 20:53:18 +0530

    serve static files

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const PORT = 3000;
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 
