a29f4679e46346deaf7bd7d7b86ecf5dd09f5f41
Abhishek Agrawal <abhi2703agrawal@gmail.com>
2022-06-25 18:41:43 +0530

    added Routes using express.js

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const PORT = 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(adminRoutes);
+
+app.use(shopRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
